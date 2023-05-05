a2b03865f3f17e18e3c22d8e76c69179002e2538
szymon <s.szymon157@gmail.com>
2023-05-05 16:51:52 +0200

    repository initialization

---
+++ src/main/java/com/flightapp/flightdataconsumer/FlightDataConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.flightapp.flightdataconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightDataConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightDataConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flightapp/flightdataconsumer/FlightDataConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightapp.flightdataconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightDataConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
